b189eebd111e2f0e1730a1721beb43655410ad36
Farhan <143311816+Farhan-Osm@users.noreply.github.com>
2025-05-01 15:32:05 +0530

    Update BestSallerBook.jsx
    add live server routes

--- src/home/BestSallerBook.jsx
+++ src/home/BestSallerBook.jsx
@@ -6,7 +6,7 @@ const BestSallerBook = () => {
     const [books, setBooks] = useState([]);
 
     useEffect( () => {
-        fetch('http://localhost:3000/all-books')
+        fetch('https://ecommerce-books-app-backend.onrender.com/all-books')
       .then(res => res.json()).then(data => setBooks(data.slice(6, 12)))
     },[])
   return (
